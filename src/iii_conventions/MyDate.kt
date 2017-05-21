43188ddd60b7a4ed98be5b7301bb89a93375d038
ITO Nobuaki <daydream.trippers@gmail.com>
2017-05-22 07:45:47 +0900

    Task 29

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -35,3 +35,11 @@ operator fun DateRange.iterator(): Iterator<MyDate> {
         }
     }
 }
+
+operator fun MyDate.plus(ti: TimeInterval): MyDate = addTimeIntervals(ti, 1)
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+
+operator fun TimeInterval.times(n: Int): RepeatedTimeInterval = RepeatedTimeInterval(this, n)
+
+operator fun MyDate.plus(rti: RepeatedTimeInterval): MyDate = addTimeIntervals(rti.timeInterval, rti.number)
